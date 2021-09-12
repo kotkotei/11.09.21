aae0978c6d47b690e7d78bea998a1718c8eb4442
kotkotei <kotkotei@yandex.ru>
2021-09-12 14:39:40 +0700

    CBOE 8 3ADANIA

---
+++ Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox3.Text);
+            double h = Convert.ToDouble(textBox2.Text);
+            double m = Convert.ToDouble(textBox4.Text);
+            double c = Math.Sqrt(2*m/(a*b*h));
+            textBox5.Text = c.ToString();
+        }
+    }
+}
